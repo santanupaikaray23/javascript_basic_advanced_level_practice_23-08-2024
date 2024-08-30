b1eda6e297acac41c793e0fd1157169814d923f3
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-08-30 17:12:41 +0530

    add all files

--- importExport/first.js
+++ importExport/first.js
@@ -10,3 +10,11 @@ dbObj.user = [
         city : 'Bhubaneswar'
     }
 ]
+
+dbObj.dbQuery = {
+    find:(table) => {return `Select * from ${table}`},
+    insert:(table,data) => {return `insert into ${table} name, city values (${data.name}, ${data})`}
+}
+
+//es5
+module.exports = dbObj;
